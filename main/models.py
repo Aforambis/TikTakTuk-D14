e5ce73399c7a518e2ca3c94cc4d54d674df7fcfc
Aforambis <rusydanmujtabaa@gmail.com>
2026-04-30 19:35:18 +0700

    Synchronization

--- main/models.py
+++ main/models.py
@@ -4,7 +4,6 @@ from django.core.validators import DecimalValidator, MinValueValidator
 from django.db import models
 from django.db.models import CheckConstraint, F, Q, UniqueConstraint
 
-
 class UserAccount(models.Model):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=100, unique=True)
@@ -15,7 +14,6 @@ class UserAccount(models.Model):
 
     def __str__(self) -> str:
         return self.username
-
 
 class Role(models.Model):
     role_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
